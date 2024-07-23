7f407f5a12a88641006ec45b34d0dccd5ab8017a
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-23 13:34:24 +0900

    0723_react

---
+++ 0723/ch01/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+// function Header() {
+//   return(
+//       <header>
+//         <h1>header</h1>
+//       </header>
+//   );
+// }
+// Header.js로 이동
+
+function App() {
+  return (
+    <div className="App">
+        <Body/>
+        <Header/>
+        <Footer/>
+      <h1>안녕하세요.</h1>
+    </div>
+  );
+}
+
+export default App;
