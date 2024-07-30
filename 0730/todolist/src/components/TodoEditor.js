a67e75e827046eaa3e82792e12d1d103c36f7cc0
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-30 11:57:32 +0900

    funtion: TodoEditor.js

--- 0730/todolist/src/components/TodoEditor.js
+++ 0730/todolist/src/components/TodoEditor.js
@@ -1,12 +1,42 @@
 import "./TodoEditor.css";
+import { useState, useRef } from "react";
 
-const TodoEditor = () => {
+const TodoEditor = ({onCreate}) => {
+    const [content, setContent] = useState("");
+    const inputRef = useRef();
+    const [isError, setIsError] = useState(false);
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+        if (isError) {
+            setIsError(false);
+        }
+    };
+    const onSubmit = () => {
+        if (!content) {
+            setIsError(true);
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    };
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    };
     return (
         <div className="TodoEditor">
             <h4>새로운 Todo 작성하기 📝</h4>
             <div className="editor_wrapper">
-                <input placeHolder="새로운 Todo..." />
-                <button>추가</button>
+                <input
+                    ref={inputRef}
+                    value={content}
+                    onChange={onChangeContent}
+                    placeholder={isError? "내용을 입력하세요" : "새로운 Todo..."} 
+                    style={{borderColor: isError? "red" : "initial"}} 
+                    onKeyDown={onKeyDown}/>
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     );
