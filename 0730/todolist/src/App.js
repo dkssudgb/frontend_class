53be83544c844b9e700d3bda140ca172cde0d2e1
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-31 11:30:59 +0900

    funtion: App.js
    Update, Delete

--- 0730/todolist/src/App.js
+++ 0730/todolist/src/App.js
@@ -4,30 +4,31 @@ import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
 import { useState, useRef } from "react";
 
-const mockTodo = [
-  {
-    id: 0,
-    isDone: false,
-    content: "React 공부하기",
-    createDate: new Date().getTime() },
-  {
-    id: 1,
-    isDone: false,
-    content: "빨래 널기",
-    createDate: new Date().getTime() },
-  {
-    id: 2,
-    isDone: false,
-    content: "노래 연습하기",
-    createDate: new Date().getTime() },
-]
+// const mockTodo = [
+//   {
+//     id: 0,
+//     isDone: false,
+//     content: "React 공부하기",
+//     createDate: new Date().getTime() },
+//   {
+//     id: 1,
+//     isDone: false,
+//     content: "빨래 널기",
+//     createDate: new Date().getTime() },
+//   {
+//     id: 2,
+//     isDone: false,
+//     content: "노래 연습하기",
+//     createDate: new Date().getTime() },
+// ]
 
 function App() {
-  const [todo, setTodo] = useState(mockTodo);
-  const idRef = useRef(3);
+  const [todo, setTodo] = useState([]);
+  const idRef = useRef(0);
+
   const onCreate = (content) => {
     const newItem = {
-      id: 0,
+      id: idRef.current,
       content, 
       isDate: false,
       createDate: new Date().getTime()
@@ -36,11 +37,22 @@ function App() {
     idRef.current += 1;
   };
 
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) => it.id === targetId ? {...it, isDone: !it.isDone} : it
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodo(todo.filter((it) => it.id !== targetId));
+  };
+  
   return (
     <div className="App">
       <Header />
       <TodoEditor onCreate={onCreate}/>
-      <TodoList todo={todo}/>
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   );
 }
