57255e2bc88a86c1b0aedbd5d68498176d178814
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-31 11:31:10 +0900

    function: TodoItem.js
    Update, Delete

--- 0730/todolist/src/components/TodoItem.js
+++ 0730/todolist/src/components/TodoItem.js
@@ -1,15 +1,23 @@
 import "./TodoItem.css";
 
-const TodoItem = ({id, content, isDone, createDate}) => {
+const TodoItem = ({id, content, isDone, createDate, onUpdate, onDelete}) => {
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    }
+    const onClickDelete = () => {
+        onDelete(id);
+    }
     return (
         <div className="TodoItem">
             <div className="checkbox_col">
-                <input type="checkbox" checked={isDone} />
+                <input className="checkboxItem" 
+                    type="checkbox" onChange={onChangeCheckbox} checked={isDone} />
             </div>
+            
             <div className="title_col">{content}</div>
             <div className="date_col">{new Date(createDate).toLocaleDateString()}</div>
             <div className="btn_col">
-                <button>삭제</button>
+                <button onClick={onClickDelete}>삭제</button>
             </div>
         </div>
     );
